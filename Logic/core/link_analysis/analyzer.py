e2c54ed91318281de4952eb9e1eba442e84c3d5a
danial <danial@smartech.ir>
2024-05-25 22:39:37 +0330

    fix

--- Logic/core/link_analysis/analyzer.py
+++ Logic/core/link_analysis/analyzer.py
@@ -108,7 +108,7 @@ class LinkAnalyzer:
 
 if __name__ == "__main__":
     # You can use this section to run and test the results of your link analyzer
-    corpus = [] ‍   # TODO: it shoud be your crawled data
+    corpus = []   # TODO: it shoud be your crawled data
     root_set = []   # TODO: it shoud be a subset of your corpus
 
     analyzer = LinkAnalyzer(root_set=root_set)
